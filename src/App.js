94a40c7e433871ad60ae7b23bed74301286bb17c
AmanPant777 <footballeramanpt65@gmail.com>
2021-07-14 15:18:43 +0530

    tesla
    npx create-react-app . --template-redux
    
    install styled components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from './comp/Home'
+import './App.css';
+import Header from './comp/Header'
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/comp/Section.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+const Section = () => {
+	return (
+		<Wrap>
+			Hi
+		</Wrap>	
+	)
+}
+
+export default Section
+
+const Wrap=styled.div`
+color:blue;
+`
