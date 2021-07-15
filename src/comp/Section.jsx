8a9d029de4cb0f256f96cc3055e61e7b025a2973
AmanPant777 <footballeramanpt65@gmail.com>
2021-07-15 14:59:25 +0530

    Make Section
    down arrow
    Buttons
    Itemtext

--- src/comp/Section.jsx
+++ src/comp/Section.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import arrow from  './down-arrow.svg'
 const Section = () => {
   return (
     <Wrap>
@@ -7,14 +8,13 @@ const Section = () => {
         <h1>Model S</h1>
         <p>Oder online for touchless delivery</p>
       </ItemText>
-	  <Button>
-	  <ButtonGroup>
-        <LeftButton>Custom Oder</LeftButton>
-        <RightButton>Existing Inventory</RightButton>
-      </ButtonGroup>
-	  <DownArrow src="images/down-arrow.svg"/>
-	  </Button>
-      
+      <Button>
+        <ButtonGroup>
+          <LeftButton>Custom Oder</LeftButton>
+          <RightButton>Existing Inventory</RightButton>
+        </ButtonGroup>
+        <DownArrow src={arrow}/>
+      </Button>
     </Wrap>
   );
 };
@@ -28,38 +28,44 @@ const Wrap = styled.div`
   background-image: url("https://images.unsplash.com/photo-1597404294360-feeeda04612e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c3BvcnRzJTIwY2FyfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80");
   background-position: center;
   background-repeat: no-repeat;
-  display:flex;
-  flex-direction:column;
-  justify-content:space-between;
-  align-items:center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 const ItemText = styled.div`
   padding-top: 15vh;
   text-aign: center;
 `;
 const ButtonGroup = styled.div`
-display:flex;
-margin-bottom:30px;
-`
-const LeftButton=styled.div`
-background-color:rgba(23,26,32,0.8);
-height:40px;
-width:256px;
-color:white;
-display:flex;
-justify-content:center;
-align-items:center;
-border-radius:100px;
-opacity:0.85;
-text-transform:uppercase;
-font-size:14px;
-cursor:pointer;
-margin:8px;
+  display: flex;
+  margin-bottom: 30px;
+`;
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 14px;
+  cursor: pointer;
+  margin: 8px;
+`;
+
+const RightButton = styled(LeftButton)``;
+
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height:40px;
+  animation:animateDown infinite 1.5s;
+  overflow-x:hidden;
 `
 
-const RightButton=styled(LeftButton)``
-
-const DownArrow=styled.div`
-margin-top:100px;
+const Button = styled.div`
+  padding: 1px;
 `
-const Button=styled.div`padding:1px;`
